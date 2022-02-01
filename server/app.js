136df887a093c883c5b69f698a119fc0b5d54946
Trevor McCubbin <mccubal13@gmail.com>
2022-02-01 14:17:46 -0500

    fix bad imports in express app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const router = require("./routes/api.js");
+const router = require("./routes/api");
+const path = require("path");
 
 // all api calls to db is in /murals
 app.use("/api", router);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const cache = require("memory-cache");
 
 router.get("/helloworld", async (req, res) => {
     res.send("Hello There");
