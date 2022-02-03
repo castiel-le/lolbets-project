7149fe79c8773f1582064efad756fab292f4a4b2
Trevor McCubbin <mccubal13@gmail.com>
2022-02-03 15:39:08 -0500

    Add base NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import logo from './testlogo.svg';
 import './App.css';
 
+import NavBar from "./components/NavBar"
+
 function App() {
   return (
     <div className="App">
+    <NavBar />
       <header className="App-header">
         <div class="svg-container">
           <img src={logo} width="64" className="App-logo" alt="logo" />
