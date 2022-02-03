77aa20d36622571a9df8fe2eeba94615ea8d2a0f
Trevor McCubbin <mccubal13@gmail.com>
2022-02-03 17:51:57 -0500

    Change logo in NavBar and clean up components into functions

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -12,43 +12,82 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
+import './NavBar.css';
+
 const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Profile', 'Friends', 'Bet History', 'Logout'];
+
+const Hamburger = (props) => {
+    return (
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={props.toggleDrawer}
+                color="inherit"
+            >
+            <MenuIcon />
+            </IconButton>
+        </Box>
+    );
+}
+
+const UserAvatar = (props) => {
+    return (
+        <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+                <IconButton onClick={props.handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={props.anchorToUser}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                open={props.openUserMenu}
+                onClose={props.handleCloseUserMenu}
+            >
+                {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={props.handleCloseUserMenu}>
+                        <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </Box>
+    );
+}
+
 
 export default class NavBar extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            anchorToNav: null,
-            anchorToUSer: null,
+            anchorToUser: null,
             openUserMenu: false,
-            openSideMenu: false,
+            sideMenuOpen: true,
         };
-        this.handleOpenNavMenu = this.handleOpenNavMenu.bind(this);
-        this.handleCloseNavMenu = this.handleCloseNavMenu.bind(this);
+
         this.handleOpenUserMenu = this.handleOpenUserMenu.bind(this);
         this.handleCloseUserMenu = this.handleCloseUserMenu.bind(this);
-    }
-
-    handleOpenNavMenu(event) {
-        this.setState({
-            anchorToNav: event.currentTarget,
-            openSideMenu: true
-        });
+        this.toggleDrawer = this.toggleDrawer.bind(this);
     }
 
     handleOpenUserMenu(event) {
         this.setState({
             anchorToUser: event.currentTarget,
             openUserMenu: true
-        });
-    }
-
-    handleCloseNavMenu() {
-        this.setState({
-            anchorToNav: null,
-            openSideMenu: false
         });
     }
 
@@ -59,108 +98,58 @@ export default class NavBar extends Component {
         });
     }
 
+    toggleDrawer() {
+        this.setState({
+            sideMenuOpen: !this.state.sideMenuOpen
+        })
+    }
+
     render() {
         return (
             <AppBar position="static">
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-                    >
-                        LOGO
-                    </Typography>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                        >
+                            <img id="navbar-logo" src="logo192.png" alt='lolbets logo'/>
+                        </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={this.handleOpenNavMenu}
-                        color="inherit"
+                        <Hamburger toggleDrawer={this.toggleDrawer} />
+                        
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
                         >
-                        <MenuIcon />
-                        </IconButton>
-                        <Menu
-                        id="menu-appbar"
-                        anchorEl={this.anchorToNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={this.state.openSideMenu}
-                        onClose={this.handleCloseNavMenu}
-                        sx={{
-                            display: { xs: 'block', md: 'none' },
-                        }}
-                        >
-                        {pages.map((page) => (
-                            <MenuItem key={page} onClick={this.handleCloseNavMenu}>
-                            <Typography textAlign="center">{page}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-                    >
-                        LOGO
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                        <Button
-                            key={page}
-                            onClick={this.handleCloseNavMenu}
-                            sx={{ my: 2, color: 'white', display: 'block' }}
-                        >
-                            {page}
-                        </Button>
-                        ))}
-                    </Box>
+                            <img src="logo192.png" alt='lolbets logo'/>
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={this.handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                            ))}
+                        </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                        <IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                        </IconButton>
-                        </Tooltip>
-                        <Menu
-                        sx={{ mt: '45px' }}
-                        id="menu-appbar"
-                        anchorEl={this.anchorToUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={this.state.openUserMenu}
-                        onClose={this.handleCloseUserMenu}
-                        >
-                        {settings.map((setting) => (
-                            <MenuItem key={setting} onClick={this.handleCloseUserMenu}>
-                            <Typography textAlign="center">{setting}</Typography>
-                            </MenuItem>
-                        ))}
-                        </Menu>
-                    </Box>
+                        <UserAvatar 
+                            openUserMenu={this.state.openUserMenu} 
+                            anchorToUser={this.state.anchorToUser} 
+                            handleOpenUserMenu={this.handleOpenUserMenu} 
+                            handleCloseUserMenu={this.handleCloseUserMenu}
+                        />
                     </Toolbar>
                 </Container>
             </AppBar>
         );
     }
 }
+
