ddc3934bc58b1571e8ba2cbd640003e59856d972
Trevor McCubbin <mccubal13@gmail.com>
2022-02-03 21:17:35 -0500

    Add React Router to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,27 @@ import logo from './testlogo.svg';
 >>>>>>> bc40c4f (create landing page)
 import './App.css';
 
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar/NavBar"
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-    <NavBar />
-      <header className="App-header">
-        <div class="svg-container">
-          <img src={logo} width="64" className="App-logo" alt="logo" />
-        </div>
-        <p id="desc">
-          Based page of LoLBets
-        </p>
-      </header>
+      <Router >
+      <NavBar user={false}/>
+        <Routes >
+          <Route path='/' element={
+            <header className="App-header">
+              <div class="svg-container">
+              <img src={logo} width="64" className="App-logo" alt="logo" />
+            </div>
+            <p id="desc">
+              Based page of LoLBets
+            </p>
+          </header>} />
+          <Route path="/test" element={<h1>Hello</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
