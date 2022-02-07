de073bf57fec4061614cd25aa3efa37e705c650e
Trevor McCubbin <mccubal13@gmail.com>
2022-02-07 14:54:27 -0500

    Fix Sequalize string

--- server/db/connection.js
+++ server/db/connection.js
@@ -8,57 +8,23 @@ const dbUrl = process.env.dbUrl;
 const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
 const database = process.env.database;
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-    host: dbUrl,
-    user: dbUsername,
-    password: dbPassword,
-    database: database,
-    port: 3306,
-    ssl: {
-        ca: serverCa
-    }
-})
 
-try {
-    connection.connect()
-} catch (exception) {
-    console.error(exception);
-}
+const { Sequelize } = require("sequelize");
 
-
-connection.query('SELECT * FROM user', function (err, rows, fields) {
-    if (err) throw err
-    rows.forEach(user => console.log('Username is ', user.username));
-})
-
-connection.end()
-
-module.exports = [
-    connection,
-];
-/*const { Sequelize } = require("sequelize");
-const fs = require('fs');
-
-const sequelize = new Sequelize({
-    "host": dbUrl,
-    "port": 3306,
-    "database": database,
-    "user": dbUsername,
-    "password": dbPassword,
-    "dialect": "mysql",
-    "ssl": true,
-    "dialectOptions": {
-        "ssl": {
-            "require": true,
-            "ca": [fs.readFileSync(__dirname + "/DigiCertGlobalRootCA.crt.pem", "utf8")]
+const sequelize = new Sequelize(
+    `mysql://${dbUsername}:${dbPassword}@${dbUrl}:3306/${database}`, {
+    dialectOptions: {
+        ssl: {
+            ca: serverCa
         }
     }
 });
 
-(async function() {try {
-    await sequelize.authenticate();
-    console.log('Connection successfully established');
-} catch(error){
-    console.error('Unable to connect: ', error);
-}})();*/
+(async function () {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection successfully established');
+    } catch (error) {
+        console.error('Unable to connect: ', error);
+    }
+})();
