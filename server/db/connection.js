f6c88c8adabd78ccfa22499bf1f9f24cdbd114c7
Jackson Kwan <jackson.kwan@dawsoncollege.qc.ca>
2022-02-07 22:28:30 -0500

    Create Badge model, Fetch from badges table

--- server/db/connection.js
+++ server/db/connection.js
@@ -9,7 +9,7 @@ const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
 const database = process.env.database;
 
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
     `mysql://${dbUsername}:${dbPassword}@${dbUrl}:3306/${database}`, {
@@ -20,11 +20,36 @@ const sequelize = new Sequelize(
     }
 });
 
-(async function () {
+/*(async function () {
     try {
         await sequelize.authenticate();
         console.log('Connection successfully established');
     } catch (error) {
         console.error('Unable to connect: ', error);
     }
+})();*/
+
+const Badge = sequelize.define('badge', {
+  // Model attributes are defined here
+  badge_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  badge_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+    // allowNull defaults to true
+  },
+  badge_image: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },  
+}, {timestamps:false});
+
+(async function (){
+    const badges = await Badge.findAll();
+    console.log(badges.every(badge => badge instanceof Badge));
+    console.log("All badges: ", JSON.stringify(badges, null, 2));
 })();
