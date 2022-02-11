489229c7c3cfe4148f7005e7339b77eab42d6b10
Trevor McCubbin <mccubal13@gmail.com>
2022-02-11 11:25:33 -0500

    Add conversion of match time to an epoch number before sending

--- server/db/dbFunctions.js
+++ server/db/dbFunctions.js
@@ -1,19 +1,19 @@
 const models = require("./sqlmodels");
 
 //Function to get all badges
-async function getBadges(){
+async function getBadges() {
     const badges = await models.Badge.findAll();
     return badges;
 }
 
 //Function to get all teams
-async function getTeams(){
+async function getTeams() {
     const teams = await models.Team.findAll();
     return teams;
 }
 
 //Function to get team data by id
-async function getTeamById(id){
+async function getTeamById(id) {
     const team = await models.Team.findAll({
         where: {
             team_id: id
@@ -23,19 +23,24 @@ async function getTeamById(id){
 }
 
 //Function to get all matches
-async function getMatches(){
+async function getMatches() {
     const matches = await models.Match.findAll();
+    matches.forEach(m => {
+        m.dataValues.match_start_time = new Date(m.dataValues.match_start_time).valueOf();
+    }
+    );
+    console.log(matches);
     return matches;
 }
 
 //Function to get all users
-async function getUsers(){
+async function getUsers() {
     const users = await models.User.findAll();
     return users;
 }
 
 //Function to get user by id
-async function getUserById(id){
+async function getUserById(id) {
     const user = await models.User.findAll({
         where: {
             user_id: id
@@ -44,4 +49,4 @@ async function getUserById(id){
     return user;
 }
 
-module.exports = {getBadges, getTeams, getTeamById, getMatches, getUsers, getUserById};
+module.exports = { getBadges, getTeams, getTeamById, getMatches, getUsers, getUserById };
