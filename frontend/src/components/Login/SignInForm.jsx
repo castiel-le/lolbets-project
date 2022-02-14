bbec4dc53b18ab60f9cdc52d2ee18834088acf76
castiel <tuananhle130902@gmail.com>
2022-02-14 11:57:18 -0500

    generate signup page

--- frontend/src/components/Login/SignInForm.jsx
+++ frontend/src/components/Login/SignInForm.jsx
@@ -119,11 +119,11 @@ export default class SignInForm extends Component {
                         color="secondary"
                         focused
                       />
+                      <FormControlLabel
+                        control={<Checkbox id="remember" style={{color: "yellow"}} value="remember" color="secondary" />}
+                        label="Remember me"
+                      />
                     </ThemeProvider>
-                    <FormControlLabel
-                      control={<Checkbox id="remember" value="remember" color="primary" />}
-                      label="Remember me"
-                    />
                     <Button
                       type="submit"
                       fullWidth
@@ -135,15 +135,10 @@ export default class SignInForm extends Component {
                     <Grid container>
                       <Grid item xs>
                         <ReactLink to={"/signup"} >
-                            <Link variant="body2">
-                            Forgot password?
-                            </Link>
-                        </ReactLink>
-                      </Grid>
-                      <Grid item xs>
-                        <ReactLink to={"/signup"} >
                             <Link href="/signup" variant="body2">
-                            Create an account
+                              <Typography sx={{color: "#f9f9f9", textAlign: "right", fontSize: "14px", margin: 2}}>
+                                Create an account
+                              </Typography>
                             </Link>
                         </ReactLink>
                       </Grid>
