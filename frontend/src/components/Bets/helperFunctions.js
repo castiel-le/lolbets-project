199ad9b8a4d4b1a725822a7ec53c9e388276cfdc
Trevor McCubbin <mccubal13@gmail.com>
2022-02-14 14:41:34 -0500

    Add function for sorting matches by date

--- frontend/src/components/Bets/helperFunctions.js
+++ frontend/src/components/Bets/helperFunctions.js
@@ -10,7 +10,7 @@ const month_names_short = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug
 export function getFormattedDate(epoch) {
     let date = new Date(epoch);
     return `${days_week[date.getDay()]} - ${month_names[date.getMonth()]} ${date.getDate()}`
-} 
+}
 
 /**
  * seperates the start time of the game into a json object
@@ -19,7 +19,7 @@ export function getFormattedDate(epoch) {
  */
 export function getGameStartTimeObject(date) {
     return {
-        'hour': (date.getHours() % 12).toString(), 
+        'hour': (date.getHours() % 12).toString(),
         'min': date.getMinutes() < 10 ? '0' + date.getMinutes().toString() : date.getMinutes().toString(),
         'period': date.getHours() < 12 ? 'AM' : 'PM'
     };
@@ -31,7 +31,6 @@ export function getGameStartTimeObject(date) {
  * @returns JSON of team details
  */
 export function getTeamObject(team) {
-    console.log(team);
     return {
         name: team.team_name,
         image: team.logo,
@@ -49,8 +48,31 @@ export function getTeamObject(team) {
 export async function fetchTeamInfo(teamID) {
     const response = await fetch(`/api/teams/${teamID}`);
     if (!response.ok) {
-      throw new Error("Error retrieving data");
+        throw new Error("Error retrieving data");
     }
     let team = await response.json();
     return team;
+}
+
+/**
+ * Sorts the matches into arrays , seperated by date
+ * @param {*} matches all matches fetched from fb
+ * @returns array of matches by date
+ */
+export function sortMatchesByDate(matches) {
+    let matchesCurrentDate = [];
+    let allDates = [];
+
+    matches.forEach(element => {
+        if (matchesCurrentDate.length === 0 || new Date(matchesCurrentDate[0].match_start_time).getDate() === new Date(element.match_start_time).getDate()) {
+            matchesCurrentDate.push(element);
+        } else {
+            allDates.push(matchesCurrentDate);
+            matchesCurrentDate = [];
+        }
+    });
+    if (matchesCurrentDate.length > 0) {
+        allDates.push(matchesCurrentDate);
+    }
+    return allDates;
 }
