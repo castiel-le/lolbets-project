a0bbb45078ed1df86efd42b5802ff4df77fa9cd7
vasht <vashtilanzrubio@gmail.com>
2022-02-15 14:19:07 -0500

    Add fetching to Team

--- frontend/src/components/Team/Team.jsx
+++ frontend/src/components/Team/Team.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import withRouter from "../../config/withRouter";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Box, Typography } from "@mui/material";
 import TeamSection from "./details/TeamSection";
@@ -9,19 +10,38 @@ const theme = createTheme();
  * Component for displaying Team page where it contains
  * a specific team's information
  */
-export default class Team extends Component {
+class Team extends Component {
     constructor(props) {
         super(props);
-        this.state = {team: null};
+        this.state = {team: {
+            team_id: null,
+            team_name: "",
+            logo: "",
+            abbreviation: "",
+            wins: null,
+            losses: null,
+        }, matches: []};
+        
+    }
+    async componentDidMount(){
+        // urls to fetch
+        const urlTeam = "/api/teams/";
+        const urlHistory = "/api/teams/history/";
+        
+        // fetch data
+        const responseTeam = await fetch(urlTeam + this.props.params.id);
+        const responseMatches = await fetch(urlHistory + this.props.params.id);
+        this.setState({team: await responseTeam.json(), matches: await responseMatches.json()});
     }
     render() {
         return(
             <ThemeProvider theme={theme}>
                 <Box display="flex" flexDirection="row" columnGap={2} style={{backgroundColor: "#1e2021"}}>
-                    <TeamSection />
+                    <TeamSection team={this.state.team} />
                     <MatchHistory />
                 </Box>
             </ThemeProvider>
         );
     }
 }
+export default withRouter(Team);
