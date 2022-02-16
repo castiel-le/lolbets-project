4fe6b74ff6bf0fba0426dc667096568e377dbce1
vasht <vashtilanzrubio@gmail.com>
2022-02-16 12:45:51 -0500

    Connect REST API  to Teams

--- frontend/src/components/Teams/Card/TeamCard.jsx
+++ frontend/src/components/Teams/Card/TeamCard.jsx
@@ -6,20 +6,21 @@ export default class TeamCard extends Component {
         // styling
         const stylingCard = { height: '100%', display: 'flex', flexDirection: 'column' };
 
+        const teamPage = "/teams/" + this.props.team.team_id; //team's team page url
         return (
-            <a href="/teams/1">
+            <a href={teamPage}>
                 <Card sx={stylingCard}>
                     <CardMedia
-                        width={200}
-                        height={200}
+                        width={300}
+                        height={300}
                         component="img"
                         image={this.props.team.logo}
                         loading="lazy"
-                        alt="logo" />
+                        alt="no logo" />
 
                     <CardContent sx={{ flexGrow: 1 }}>
                         <Typography variant="h5" component="h2">
-                            {this.props.team.name}
+                            {this.props.team.team_name}
                         </Typography>
                     </CardContent>
                 </Card>
