170b29d446886c11cfefe5450d2ee302e98b78c0
castiel <tuananhle130902@gmail.com>
2022-03-03 14:36:23 -0500

    change digicert file into env var

--- server/db/connection.js
+++ server/db/connection.js
@@ -9,7 +9,7 @@ const dbUrl = process.env.dbUrl;
 const dbUsername = process.env.dbUsername;
 const dbPassword = process.env.dbPassword;
 const database = process.env.database;
-const serverCa = [fs.readFileSync(__dirname + "/DigiCertGlobalRootCA.crt.pem", "utf8")];
+const serverCa = process.env.certificate;
 
 const sequelize = new Sequelize(
     `mysql://${dbUsername}:${dbPassword}@${dbUrl}:3306/${database}`, {
