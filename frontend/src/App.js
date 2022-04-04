82a9ffad84312f25c248c64c2a0fcec8f79f1402
Marian Ruth Lina <gmmisty03@gmail.com>
2022-04-04 17:59:32 +0000

    landing page to staging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 
 import BetsPage from './components/Bets/BetsPage';
 import NavBar from "./components/NavBar/NavBar"
@@ -14,12 +12,14 @@ import UserBetHistory from './components/UserBetHistory/UserBetHistroy';
 import SearchResults from './components/Leaderboard/SearchResults'
 import { SnackbarContainer } from './components/Snackbar/SnackbarContext';
 import SnackbarAlert from './components/Snackbar/SnackbarAlert';
-import Logout from "./components/Logout/Logout";
+import { Logout } from '@mui/icons-material';
+import LandingPage from './components/LandingPage/Landingpage';
+
 
 function App() {
-    const [user, setUser] = useState({id: null, role: null, coins: 0, isLoaded: false});
+    const [user, setUser] = useState({ id: null, role: null, coins: 0, isLoaded: false });
     const location = useLocation();
-    
+
     /**
      * Function that sets the user state to the currently logged in user.
      * It fetches from the API and if it returns an object, that means that the user
@@ -30,14 +30,14 @@ function App() {
     const verifyUser = async () => {
         const userURL = "/userinfo";
         const userVerified = await fetch(userURL);
-        
-        if(userVerified.ok) {
+
+        if (userVerified.ok) {
             const fetchedUser = await userVerified.json();
-            setUser({id: fetchedUser.id, role: fetchedUser.role, coins: fetchedUser.coins, isLoaded: true});
+            setUser({ id: fetchedUser.id, role: fetchedUser.role, coins: fetchedUser.coins, isLoaded: true });
             return;
         }
 
-        setUser({id: null, role: null, coins: 0, isLoaded: true});
+        setUser({ id: null, role: null, coins: 0, isLoaded: true });
     }
 
     /**
@@ -55,30 +55,23 @@ function App() {
                 <NavBar user={user} />
                 <Suspense fallback={<Loading />}>
                     <Routes >
-                        <Route path='/' element={
-                            <header className="App-header">
-                                <div className="svg-container">
-                                    <img src={logo} width="64" className="App-logo" alt="logo" />
-                                </div>
-                                <p id="desc">
-                                    Based page of LoLBets
-                                </p>
-                            </header>} />
-                        <Route path='/bets' element={<BetsPage user={user} updateUser={verifyUser}/>} />
+                        <Route path='/' element={<LandingPage />} />
+                        <Route path='/bets' element={<BetsPage user={user} updateUser={verifyUser} />} />
                         <Route path='/bets/:id' element={<h1> Bet Number </h1>} />
                         <Route path='/bets/create' element={<h1> Create Bet </h1>} />
                         <Route path='/bets/edit/:id' element={<h1> Edit Bet Number </h1>} />
-                        <Route path='/profile' element={<Profile user={user} key="profile"/>} />
-                        <Route path='/user/:id' element={<Profile user={user} key="user"/>} />
-                        <Route path='/user/:id/history' element={<UserBetHistory  user={user} />} />
-                        <Route path='/leaderboard' element={<Leaderboard  user={user} />} />
-                        <Route path='/teams' element={<Teams  user={user} />} />
-                        <Route path='/teams/:id' element={<Team  user={user} />} />
+                        <Route path='/profile' element={<Profile user={user} key="profile" />} />
+                        <Route path='/user/:id' element={<Profile user={user} key="user" />} />
+                        <Route path='/user/:id/history' element={<UserBetHistory user={user} />} />
+                        <Route path='/leaderboard' element={<Leaderboard user={user} />} />
+                        <Route path='/teams' element={<Teams user={user} />} />
+                        <Route path='/teams/:id' element={<Team user={user} />} />
                         <Route path='/user/search' element={<SearchResults />} />
                         <Route path='/teams/search/:teamname' element={<h1> Search Team </h1>} />
                         <Route path='/logout' element={<Logout />} />
-                        <Route path='/404' element={<h1 style={{color: "white"}}> Not Found </h1>} />
-                        <Route path='*' element={<h1 style={{color: "white"}}> Not Found </h1>} />
+                        <Route path='/404' element={<h1 style={{ color: "white" }}> Not Found </h1>} />
+                        <Route path='*' element={<h1 style={{ color: "white" }}> Not Found </h1>} />
+
                     </Routes>
                 </Suspense>
             </div>
