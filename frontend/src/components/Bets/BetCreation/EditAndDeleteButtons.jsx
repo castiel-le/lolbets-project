0134253ca657097c86d71e0dfc44128fd2da05e3
Trevor McCubbin <mccubal13@gmail.com>
2022-04-05 23:56:48 +0000

    Bet Button Redirect

--- frontend/src/components/Bets/BetCreation/EditAndDeleteButtons.jsx
+++ frontend/src/components/Bets/BetCreation/EditAndDeleteButtons.jsx
@@ -1,7 +1,29 @@
-import { Tooltip } from "@mui/material";
+import { Tooltip, styled, Button } from "@mui/material";
 import { Delete, Edit } from "@mui/icons-material";
-import { BetButtonStyle } from "../styledElements";
 import { FlexBoxColumn } from "../../customUIComponents";
+
+/**
+ * Bet button in bet view styled
+ */
+const BetButtonStyle = styled(Button)({
+    variant: 'contained',
+    margin: 'auto',
+    textDecoration: 'underline',
+    boxShadow: 'unset',
+    borderRadius: 16,
+    backgroundColor: 'unset',
+    color: '#f9f9f9',
+    fontFamily: 'Lemon-Milk-Bold',
+    height: '45px',
+    width: '85px',
+    fontSize: '26px',
+    marginLeft: 'auto',
+    ":hover": {
+        textDecoration: 'underline',
+        backgroundColor: '#f9f9f9',
+        color: '#111111'
+    }
+});
 
 export default function EditAndDeleteButtons({onEdit, onDelete}) {
 
